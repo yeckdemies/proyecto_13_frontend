5fe46d43b52f69de4bc4bbf0de345bea21c2b8e8
Yolanda <yeckdemies@gmail.com>
2025-05-05 23:24:44 +0200

    Deploy

--- src/api/apiClient.js
+++ src/api/apiClient.js
@@ -2,8 +2,8 @@ import axios from 'axios';
 import { showLoader, hideLoader } from '../utils/loaderEvents';
 
 const apiClient = axios.create({
-  //const API_URL = 'https://proyecto-13-backend.onrender.com/api/v1/users';
-  baseURL: 'http://localhost:3000/api/v1',
+  //baseURL: 'http://localhost:3000/api/v1',
+  baseURL: 'https://proyecto-13-backend.onrender.com/api/v1',
 });
 
 apiClient.interceptors.request.use(config => {
